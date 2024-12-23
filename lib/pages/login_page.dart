9c4d086989f875421830a4ceb1b60d7aeb5b310a
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-12-24 01:46:53 +0300

    finish login page ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,6 +13,9 @@ class LoginPage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: Column(
           children: [
+            const Spacer(
+              flex: 2,
+            ),
             Image.asset("assets/Images/scholar.png"),
             const Text(
               "Scholar Chat",
@@ -21,19 +24,55 @@ class LoginPage extends StatelessWidget {
                 fontFamily: "pacifico",
               ),
             ),
-            const Text(
-              "LOGIN",
-              style: TextStyle(
-                fontSize: 24,
-              ),
+            const Spacer(
+              flex: 1,
+            ),
+            const Row(
+              children: [
+                Text(
+                  "LOGIN",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
             ),
             const CustomTextField(
               hintText: "Email",
             ),
+            const SizedBox(
+              height: 10,
+            ),
             const CustomTextField(
               hintText: "Password",
             ),
+            const SizedBox(
+              height: 20,
+            ),
             const CustomButton(),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "don't have an account ?  ",
+                ),
+                Text(
+                  " REGISTER",
+                  style: TextStyle(
+                    color: Color(0xffC7EDE6),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(
+              flex: 3,
+            ),
           ],
         ),
       ),
