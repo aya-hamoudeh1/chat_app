0f488aeb0dc09a3332341bbf053ad790ead011fb
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2024-12-24 01:32:20 +0300

    create custom_text_field.dart & custom_button.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/custom_button.dart';
+import 'package:chat_app/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,36 +9,33 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff2B475E),
-      body: Column(
-        children: [
-          Image.asset("assets/Images/scholar.png"),
-          const Text(
-            "Scholar Chat",
-            style: TextStyle(
-              fontSize: 32,
-              fontFamily: "pacifico",
-            ),
-          ),
-          const Text(
-            "LOGIN",
-            style: TextStyle(
-              fontSize: 24,
-            ),
-          ),
-          const TextField(
-            decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.white,
-                ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          children: [
+            Image.asset("assets/Images/scholar.png"),
+            const Text(
+              "Scholar Chat",
+              style: TextStyle(
+                fontSize: 32,
+                fontFamily: "pacifico",
               ),
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                color: Colors.white,
-              )),
             ),
-          ),
-        ],
+            const Text(
+              "LOGIN",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            const CustomTextField(
+              hintText: "Email",
+            ),
+            const CustomTextField(
+              hintText: "Password",
+            ),
+            const CustomButton(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      width: double.infinity,
+      height: 60,
+      child: const Center(
+        child: Text(
+          "LOGIN",
+          style: TextStyle(
+            color: Color(0xff2B475E),
+          ),
+        ),
+      ),
+    );
+  }
+}
