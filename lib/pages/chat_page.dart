c6cbaee13ccebfb9bb48023eb34c809f48a755b1
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-03-08 01:26:36 +0300

    firestore database

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -10,7 +10,6 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: kPrimaryColor,
