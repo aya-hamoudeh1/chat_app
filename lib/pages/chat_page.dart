e64f5d6e2736e805ac256104b50f6bc159228e3c
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-03-08 00:51:12 +0300

    add text field to chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -2,7 +2,6 @@ import 'package:chat_app/constants.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
 
-
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
@@ -27,10 +26,40 @@ class ChatPage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return const ChatBubble();
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return const ChatBubble();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Send Message",
+                hintStyle: const TextStyle(
+                  color: kPrimaryColor,
+                ),
+                suffixIcon: const Icon(
+                  Icons.send,
+                  color: kPrimaryColor,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(
+                    color: kPrimaryColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
